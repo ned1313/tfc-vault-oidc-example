a5e4b303bf458f71c7f8dd39b10259bd704d0a26
ned1313 <ned.bellavance@gmail.com>
2022-10-21 10:18:07 -0400

    add output and remove file

--- main.tf
+++ main.tf
@@ -14,10 +14,11 @@ data "vault_generic_secret" "taco" {
 
 data "environment_variables" "all" {}
 
-resource "local_file" "workspace_token" {
+# Uncomment this to debug the workspace token
+/*resource "local_file" "workspace_token" {
   content  = data.environment_variables.all.items["TFC_WORKLOAD_IDENTITY_TOKEN"]
   filename = "~/tfctoken"
-}
+}*/
 
 
 
